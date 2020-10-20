3c47e94ec3e2edeb58fc60ed120834ab5f0ef046
Jeremy Kun <j2kun@users.noreply.github.com>
2020-10-20 12:03:17 -0700

    add q command

--- example_data.sh
+++ example_data.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.csv | gawk -F, 'NR>1{split($1,date,"T"); arr[date[1]]++}END{PROCINFO["sorted_in"]="@ind_str_asc"; for (a in arr) {j++; print j, arr[a]}}'
+curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.csv |
+    q -H -d ',' 'select date(time), count(*) from - group by date(time) order by date(time)' |
+
+# q command can be replaced by this awk command if q is not installed
+#gawk -F, 'NR>1{split($1,date,"T"); arr[date[1]]++}END{PROCINFO["sorted_in"]="@ind_str_asc"; for (a in arr) {j++; print j, arr[a]}}'
+
+    awk -F, '{j++; print j, $2}'
